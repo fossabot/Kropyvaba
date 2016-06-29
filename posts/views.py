ad9cf45128674fad9abcc6002ecf5f8f0a391fbd
SHooZ <SHooZ@ex.ua>
2016-06-29 21:49:18 +0300

    Added page and catalog support

--- posts/views.py
+++ posts/views.py
@@ -12,67 +12,50 @@ def index(request):
 def ret_post(request, post_id):
 	this_post = Post.objects.filter(id = post_id)
 	if len(this_post) == 0:
-	    this_post = Post.objects.filter(id = 1)
-	board_list = Board.objects.all()
-	curr_board = board_list.filter(id = this_post[0].board.id)
-	if this_post[0].thread != 0:
-		topic = Post.objects.filter(id = this_post[0].thread)
+		this_post = Post.objects.filter(id = 1)
+		return HttpResponseRedirect('/post-'+str(this_post[0].id)) #if post with requested id dont exist, return to post with id 1
 	else:
-		topic = Post.objects.filter(id = this_post[0].id)
-		this_post = 0
-	posts_list = Post.objects.filter(thread = topic[0].id)
-	if request.method == 'POST': #work with forms.
-		post_form = PostForm(request.POST, request.FILES)
-		if post_form.is_valid():
-			new_post = post_form.save(commit=False)
-			if len(new_post.author) != 0 or len(new_post.body) != 0 or len(request.FILES) != 0:
-				if len(new_post.author) == 0:
-					new_post.author = 'Хтось'
-				new_post.pub_date = timezone.now()
-				new_post.board = curr_board[0]
-				new_post.save()
-				return HttpResponseRedirect('/post-'+str(post_id))
-	else:
-		post_form = PostForm()
-	return render(request, 'posts/post.html', {'board_list': board_list, 'topic' : topic[0], 'post_form': post_form, 'posts_list': posts_list})
+		board_list = Board.objects.all()
+		curr_board = board_list.filter(id = this_post[0].board.id)
+		if this_post[0].thread != 0:
+			oppost = Post.objects.filter(id = this_post[0].thread)
+			return HttpResponseRedirect('/post-'+str(oppost[0].id)+'#'+str(this_post[0].id))
+		else:
+			oppost = Post.objects.filter(id = this_post[0].id)
+			this_post = 0
+		posts_list = Post.objects.filter(thread = oppost[0].id)
+		if request.method == 'POST': #work with forms.
+			post_form = PostForm(request.POST, request.FILES)
+			if post_form.is_valid():
+				new_post = post_form.save(commit=False)
+				if len(new_post.author) != 0 or len(new_post.body) != 0 or len(request.FILES) != 0:
+					if len(new_post.author) == 0:
+						new_post.author = 'Хтось'
+					new_post.pub_date = timezone.now()
+					new_post.board = curr_board[0]
+					new_post.save()
+					return HttpResponseRedirect('/post-'+str(post_id))
+		else:
+			post_form = PostForm()
+	return render(request, 'posts/post.html', {'curr_board' : curr_board, 'board_list': board_list, 'topic' : oppost[0], 'post_form': post_form, 'posts_list': posts_list})
 
-def ret_board(request, board_name):
+def ret_board_with_catalog(request, board_name):
+	return ret_board(request, board_name)
+
+def ret_board_page(request, board_name, page_num):
+	return ret_board(request, board_name, page_num)
+	
+def ret_board(request, board_name, page_num = 1):
 	board_list = Board.objects.filter(name = board_name)
 	board_id = board_list[0].id
-	"""all_post = Post.objects.filter(board = board_id)
-	temp_list = all_post.order_by('-pub_date')
+	all_post = Post.objects.filter(board = board_id)
 	topics_list = []
-	for post in temp_list:
-		unik = True
-		if post.thread == 0:
-			reply = post.id
-		else:
-			reply = post.thread
-		for topic in topics_list:
-			if reply != topic.id and unik == True:
-				unik = True
-			else:
-				unik = False
-				break
-		if unik == True:
-			temp = Post.objects.filter(id = reply)
-			topics_list.append(temp[0])
+	for thread in ThreadPost.objects.all():
+		if thread.op.board.id == board_id:
+			topics_list.append(thread)
 	posts_list = Post.objects.exclude(thread = 0).order_by('-id')
-	temp_list = []
-	for topic in topics_list:
-	    temp = 0
-	    for post in posts_list:
-	        if post.thread == topic.id and temp <= 5:
-	            temp_list.append(post)
-	            temp+=1
-	posts_list = temp_list"""
 	board_list = Board.objects.order_by('description')
 	curr_board = board_list.filter(id = board_id)
-	"""for topic in topics_list:
-		for post in all_post:
-			if post.thread == topic.id:
-				topic.posts +=1
-		topic.posts -=3"""
 	if request.method == 'POST':
 		post_form = PostForm(request.POST, request.FILES)
 		if post_form.is_valid():
@@ -108,6 +91,8 @@ def ret_board(request, board_name):
 	else:
 		post_form = PostForm()
 		thread_form = ThreadForm()
-	#topics_list = topics_list[:12]
-	#return render(request,  'posts/board.html', {'curr_board': curr_board,'board_list': board_list, 'thread_form': thread_form, 'topics_list': topics_list, 'posts_list': posts_list})
-	return render(request, 'posts/board.html', {'curr_board': curr_board,'board_list': board_list, 'thread_form': thread_form, 'post_form': post_form})
+	topics_list = topics_list[(12*int(page_num)):(12*int(page_num)+12)]
+	next = int(page_num)+1
+	prev = int(page_num)-1
+	last = 100
+	return render(request, 'posts/board.html', {'next' : next, 'prev' : prev, 'page_num': page_num, 'last' : last, 'curr_board': curr_board,'board_list': board_list, 'thread_form': thread_form, 'post_form': post_form, 'topics_list': topics_list, 'posts_list': posts_list})
